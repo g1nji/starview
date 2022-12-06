11d2cac822349c942e27f378597b6943358b3da5
jiwon <jiwon@DESKTOP-AR4UU3O>
2022-12-06 17:20:45 +0900

    221206 - 지원 - sunset

--- StarView/src/main/java/jiwon/controller/SunsetController.java
+++ StarView/src/main/java/jiwon/controller/SunsetController.java
@@ -6,7 +6,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -14,7 +19,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jiwon.dto.Sunset;
 import jiwon.service.face.SunsetService;
 
 @Controller
@@ -25,30 +36,32 @@ public class SunsetController {
 	
 	@Autowired private SunsetService sunsetService;
 
-	@RequestMapping("/sunset/time")
-	public void sunsetview() throws IOException {
+	@RequestMapping(value="/sunset/time", method=RequestMethod.GET)
+	public void sunsetview(@RequestParam String loc) throws IOException {
+		
+		// 현재 날짜를 원하는 문자열 포맷으로 변환
+		Date date = Calendar.getInstance().getTime();  
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");  
+		String strDate = dateFormat.format(date); 
 		
 		//서비스키
 		String serviceKey = "uej3x%2FSsIAytAndt4BMIKuPLeCRuvBJL1aMC%2BiIe3gzxsas6kdk0hv7SLipcQPprTnEOdxDKToBjNSPqXw2nrQ%3D%3D";
 		
 		//API URL
-		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService");
+		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B090041/openapi/service/RiseSetInfoService/getAreaRiseSetInfo");
 		//서비스키 추가하기
 		urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=" + serviceKey);
-		//연도 설정
-		urlBuilder.append("&" + URLEncoder.encode("stdrYear", "UTF-8") + "=" + URLEncoder.encode("2022", "UTF-8"));
-	
-		//요청 자료 데이터타입 XML로 설정하기
-		urlBuilder.append("&" + URLEncoder.encode("format", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
-		//페이지 출력 수
-		urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("5", "UTF-8"));
-		//페이지 번호
-		urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
-   
-//		urlBuilder.append("&" + URLEncoder.encode("pnu", "UTF-8") + "=" + URLEncoder.encode(pnu_code, "UTF-8")); //pnu_code
 		
+		// 오늘 날짜 (yyyymmdd)
+		urlBuilder.append("&" + URLEncoder.encode("locdate", "UTF-8") + "=" + URLEncoder.encode(strDate, "UTF-8"));
+		// 조회 위치
+		urlBuilder.append("&" + URLEncoder.encode("location", "UTF-8") + "=" + URLEncoder.encode(loc, "UTF-8"));
+		
+
+		logger.info(urlBuilder.toString());
 		
 		Map<String, Object> map = new HashMap<String, Object>();
+		
 		
 		
 		//get방식으로 전송해서 파라미터 받아오기
@@ -78,6 +91,31 @@ public class SunsetController {
 		//StringBuilder로 위에 파라미터 더한 값을 toString으로 불러오기
 		//log객체 이용해서 XML 형식 출력하기
 		logger.info(sb.toString());
+		
+		// XML을 JSON으로 변환
+//		JSONObject xmlObject = XML.toJSONObject(sb.toString());
+//		String xmlJsonString = xmlObject.toString();
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> resultMap = new HashMap<>();
+        
+        //typereference이용해서 map객체로 변환
+//        resultMap = objectMapper.readValue(xmlJsonString, new TypeReference<Map<String, Object>>(){});
+        Map<String, Object> dataResponse = (Map<String, Object>) resultMap.get("response");
+        Map<String, Object> body = (Map<String, Object>) dataResponse.get("body");
+        LinkedHashMap<String, Object> items  = (LinkedHashMap<String, Object>) body.get("items");
+        Map<String, Object> item = (Map<String, Object>) items.get("item");
+        
+        logger.info(" item : {} ", item);
+        
+        Sunset sunset = new Sunset();
+        sunset.setSunsetNum(Integer.parseInt(item.get("sunset").toString()));
+        sunset.setLatitudeNum(Double.parseDouble(item.get("latitudeNum").toString()));
+        sunset.setLongitudeNum(Double.parseDouble(item.get("longitudeNum").toString()));
+        sunset.setLocdate(strDate);
+        sunset.setLocation(loc);
+      
+        logger.info(" sunset : {} ", sunset);
 		
 	}
 }
--- StarView/src/main/java/jiwon/dto/Sunset.java
+++ StarView/src/main/java/jiwon/dto/Sunset.java
@@ -4,15 +4,15 @@ import java.util.Date;
 
 public class Sunset {
 	
-	private Date locdate;	//날짜
+	private String locdate;	//날짜
 	private String location;	//지역
-	private int longitudeNum;	//경도
-	private int latitudeNum;	//위도
+	private Double longitudeNum;	//경도
+	private Double latitudeNum;	//위도
 	private int sunsetNum;		//일몰
 	
 	public Sunset() { }
 
-	public Sunset(Date locdate, String location, int longitudeNum, int latitudeNum, int sunsetNum) {
+	public Sunset(String locdate, String location, Double longitudeNum, Double latitudeNum, int sunsetNum) {
 		super();
 		this.locdate = locdate;
 		this.location = location;
@@ -27,11 +27,11 @@ public class Sunset {
 				+ ", latitudeNum=" + latitudeNum + ", sunsetNum=" + sunsetNum + "]";
 	}
 
-	public Date getLocdate() {
+	public String getLocdate() {
 		return locdate;
 	}
 
-	public void setLocdate(Date locdate) {
+	public void setLocdate(String locdate) {
 		this.locdate = locdate;
 	}
 
@@ -43,19 +43,19 @@ public class Sunset {
 		this.location = location;
 	}
 
-	public int getLongitudeNum() {
+	public Double getLongitudeNum() {
 		return longitudeNum;
 	}
 
-	public void setLongitudeNum(int longitudeNum) {
+	public void setLongitudeNum(Double longitudeNum) {
 		this.longitudeNum = longitudeNum;
 	}
 
-	public int getLatitudeNum() {
+	public Double getLatitudeNum() {
 		return latitudeNum;
 	}
 
-	public void setLatitudeNum(int latitudeNum) {
+	public void setLatitudeNum(Double latitudeNum) {
 		this.latitudeNum = latitudeNum;
 	}
 
@@ -66,8 +66,7 @@ public class Sunset {
 	public void setSunsetNum(int sunsetNum) {
 		this.sunsetNum = sunsetNum;
 	}
-	
-	
+
 
 
 
